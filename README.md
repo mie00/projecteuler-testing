01445105d19237b6e961f84cb697162b2bc1f3c4
mie00 <mielawadi@gmail.com>
2015-12-27 20:22:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# ProjectEuler testing
+
+This project is user to help testing while solving [projecteuler.net](projecteuler.net) problems. It only tests the application against the examples provided with the problem.
+Have fun :)
+
+## Usage
+
+`./pet.py -n problem_number file`
+
+## License
+
+[MIT](./LICENSE) © 2015 [Mohamed Elawadi](http://www.github.com/mie00)
---
+++ pet.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+
+import argparse, json, sys, traceback, re
+
+class Case:
+	def __init__(self, f, i, o):
+		self.f = f
+		self.i = i
+		self.o = o
+
+	def sol(self, s, e = None):
+		self.s = s
+		self.e = e
+		self.failed = self.o != self.s
+		self.error = self.e != None
+		if self.error:
+			self.case = 0
+		elif self.failed:
+			self.case = 1
+		else:
+			self.case = 2
+
+	def __str__(self):
+		return "(function: %s, input: %s, output: %s, expected_output: %s, error: %s)" % (self.f, self.i, self.s, self.o, self.e)
+
+	def __repr__(self):
+		return self.__str__()
+
+def test_io(module, cases):
+	for case in cases:
+		yield check_io(module, *case)
+
+def check_io(module, i, o, fn='main'):
+	e = None
+	sol = None
+	try:
+		sol = getattr(module,fn)(i)
+	except Exception as ex:
+		e = sys.exc_info()
+	case = Case(fn, i, o)
+	case.sol(sol, e)
+	return case
+
+def main():
+	parser = argparse.ArgumentParser(description="Test your solution of a projecteuler's problem.")
+	parser.add_argument('-n','--number', metavar='problem_number', type=int,help='the number of the problem')
+	parser.add_argument('file', metavar='file', type=str,help='the solution')
+
+	args = parser.parse_args()
+
+	mod = args.file.rsplit('/',1)
+	if not args.number:
+		m = re.search('\d+',mod[1])
+		if not m:
+			raise Exception("I cannot guess the problem number please specify it with -n.")
+		args.number = m.group(0)
+
+	sys.path.append(mod[0])
+	module = __import__(mod[1][:-3])
+
+	with open('tests.json') as f:
+		tests = json.load(f)
+	try:
+		cases = tests[str(args.number)]
+	except KeyError:
+		raise Exception("There is no cases for this problem.")
+
+	cs = test_io(module, cases)
+	repr_cases(list(cs))
+
+class TCs:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def repr_cases(cases):
+	HR = '-'*80
+	COLORS = {2:TCs.OKGREEN, 1:TCs.WARNING, 0:TCs.FAIL}
+	REPR_CHAR = {2:'.', 1:'F', 0:'E'}
+	count = [0]*3
+	chars = ""
+	for i in cases:
+		count[i.case]+=1
+		chars += "%s%s%s"%(COLORS[i.case],REPR_CHAR[i.case],TCs.ENDC)
+	print chars
+	for i in cases:
+		if i.error:
+			print HR
+			print "input %s for the function %s"%(i.i, i.f)
+			traceback.print_exception(*i.e)
+		elif i.failed:
+			print HR
+			print "input %s for the function %s\n%s exepected but got %s" %(i.i, i.f, i.o, i.s)
+	print HR
+	print "%i/%i passed"%(count[2],sum(count))
+
+
+
+if __name__ == "__main__":
+	main()
